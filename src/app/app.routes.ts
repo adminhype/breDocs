545246a9a523b9a471a06ef64f7d8b3816732797
adminhype <ado.designs@outlook.de>
2025-12-04 14:22:39 +0100

    feat: main layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { WelcomePlaceholderComponent } from './features/content/welcome-placeholder/welcome-placeholder.component';
+import { DocViewerComponent } from './features/content/doc-viewer/doc-viewer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',component: MainLayoutComponent,children: [
+        {
+            path: '',
+            component: WelcomePlaceholderComponent
+        },
+        {
+            path: ':slug',
+            component: DocViewerComponent
+        }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarContainerComponent } from '../../features/sidebar/sidebar-container/sidebar-container.component';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  imports: [RouterOutlet, SidebarContainerComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
